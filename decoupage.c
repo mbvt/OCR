26727844d509b0dd2ecf360e5f3a6aa5befcd1db
Thomas Boucard <thomas.boucard39@gmail.com>
2016-10-22 22:28:15 +0200

    récupération de la zone de texte

--- decoupage.c
+++ decoupage.c
@@ -13,27 +13,34 @@ Image* cut(Image *img, Categorie cat, int i_min, int i_max, int j_min, int j_max
         return new_img;
 }
 
-Image *cut_column(Image* img)
+int not_empty(Image *img, int i, int j)
 {
-  Image* a;
-  int i_min = -1;
-  int i_max = -1;
-  int j_min = 0;
-  int j_max = img-> w;
-  for(int i = 0; i < img->h; i++)
-    {
-      int res = 0;
-      for(int j = 0 ; j < img->w; j++)
-	{
-	  res = res + get_pixel(Image* img, i, j);;
-	}   
-      if(res > 0 && i_min == -1)
-	i_min = i;
-      if(res = 0 && i_min == -1)
-	{
-	  i_max = i-1;
-	  a = cut_image(Image* img, COLONNE, i_min, i_max, j_min, j_max);
-	}
-    }
-  return a; 
+	assert(img && (
+			i == -1 && j>=0 && j<img->w ||
+			j == -1 && i>=0 && i<img->h);
+	int r = 0;
+	if(i==-1)
+		for(i=0;i<img->h && r == 0;i++)
+			r = get_pixel(img,i,j);
+	else
+		for(j=0;j<img->w && r == 0;j++)
+			r = get_pixel(img,i,j);
+	return r;
+}
+
+void get_text(Image * img)
+{
+	int i_min = -1;
+	int i_max = -1;
+	int j_min = -1;
+	int j_max = -1;
+	for(int i=0; i<img->h && i_min == -1; ++i)
+		i_min=not_empty(img,i,-1)?i:-1;
+	for(int i=img->h-1; i>0 && i_max == -1; --i)
+		i_max=not_empty(img,i,-1)?i:-1;
+	for(int j=0; j<img->w && j_min == -1; ++j)
+		j_min=not_empty(img,-1,j)?j:-1;
+	for(int j=img->w-1; j>0 && j_max == -1; --j)
+		j_max=not_empty(img,-1,j)?j:-1;
+	set_text(img,i_min,i_max,j_min,j_max);
 }
